ee5ed5a2e650d80f854f990431bb5eaca026c019
Edgar Nyamekye Agongo <kwakuedgar@gmail.com>
2024-01-19 08:29:14 -0800

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,13 +4,14 @@ package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+
 # Install python3-pip (22.0.2)
 package {'python3-pip':
-  ensure   => '22.0.2',
   provider => 'pip3'
 }
-# Install werkzeug(3.0.1)
+
+# Install werkzeug(2.1.1)
 package {'werkzeug':
-  ensure   => '3.0.1',
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
