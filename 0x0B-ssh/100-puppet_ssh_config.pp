68e6323d3c6016c80ad30b5dd6a437867d1a1bcf
Edgar Nyamekye Agongo <kwakuedgar@gmail.com>
2024-01-21 06:38:38 -0800

    SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,14 @@
-# Puppet set up
+# Seting up my client config file
+include stdlib
 
-file { 'Turn off passwd auth':
+file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
   replace => true,
 }
 
-file { 'Declare identity file':
+file_line { 'Delare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '     IdentityFile ~/.ssh/school',
